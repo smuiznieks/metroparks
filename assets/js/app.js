f7f030b821c618402259e85d3ee9471e3dbf48cf
Selga Muiznieks <selga.muiznieks@gmail.com>
2018-04-25 10:37:25 -0400

    add Google Map

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
-// Placeholder
+var map;
+var cle;
+var marker;
+var infowindow;
+var service;
+
+// Initiates Google map
+function initMap() {
+    cle = {lat: 41.505493, lng: -81.681290};
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: cle
+    });
+};
+
+window.onload = function() {
+    initMap();
+};
